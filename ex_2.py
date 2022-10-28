4f7109b9f46f15c7d1397188eedf3ec1781d2f3c
Annashevchuk <sevcukanna960@gmail.com>
2022-10-29 02:14:03 +0300

    hm_2/ex_2

---
+++ ex_2.py
@@ -0,0 +1,9 @@
+number = int(input("number-"))
+number_1 = number // 1000
+number_2 = number % 1000 // 100
+number_3 = number % 100 // 10
+number_4 = number % 10
+print(number_1 * number_2 * number_3 * number_4)
+
+
+
