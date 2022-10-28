fdec23e40ccf2983358027235c4a392cc5116bd3
Annashevchuk <sevcukanna960@gmail.com>
2022-10-29 02:07:56 +0300

    lab/ex_2

---
+++ ex_2.py
@@ -0,0 +1,12 @@
+number = int(input("number-"))
+number_1 = number % 10
+number_2 = number % 100 // 10
+number_3 = number // 100
+print (number_3)
+print (number_2)
+print (number_1)
+print(number_1 + number_2 + number_3)
+
+
+
+
