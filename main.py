70772a87f9ecc3eb159add9a15f0cff50abf902e
Annashevchuk <sevcukanna960@gmail.com>
2022-11-08 17:33:37 +0200

    ex5

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-number1 = int(input("number1="))
-number2 = int(input("number2="))
-if number1 >  number2:
-    print(number2)
-if number1 < number2:
-    print(number1)
-if number1 == number2:
-    print(number1)
+x = float(input('x->'))
+y = float(input('y->'))
+print('#>-----------<Menu>-----------<#')
+print(f'|   + : Show sum for {x} and {y}   |')
+print(f'|   - : Show dif for {x} and {y}   |')
+print(f'|   * : Show mul for {x} and {y}   |')
+print(f'| avg : Show avg for {x} and {y}   |')
+print('#>----------------------------<#')
+action = input('action->')
+if action == '+':
+    print(f'{x} + {y} = {x + y}')
+elif action == '-':
+    print(f'{x} - {y} = {x - y}')
+elif action == '*':
+    print(f'{x} * {y} = {x * y}')
+elif action == 'avg':
+    print(f'Avg = {(x+y)/2}')
