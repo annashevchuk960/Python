43624852e2b167c8009949a90778f11a2f565ea3
Annashevchuk <sevcukanna960@gmail.com>
2022-11-08 16:31:51 +0200

    ex4

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 number1 = int(input("number1="))
 number2 = int(input("number2="))
-if number1 < number2:
+if number1 >  number2:
     print(number2)
-if number1 > number2:
+if number1 < number2:
     print(number1)
 if number1 == number2:
     print(number1)
