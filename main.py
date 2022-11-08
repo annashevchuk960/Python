cdaa9ad97de54ec61725173d9ba70515f29c5e63
Annashevchuk <sevcukanna960@gmail.com>
2022-11-08 18:30:12 +0200

    hm2

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 x = float(input('x->'))
 y = float(input('y->'))
 z = float(input('z->'))
-print('#>-----------<Menu>-----------------------<#')
-print(f'|   + : Show sum for {x} and {y} and {z}   |')
-print(f'|   * : Show mul for {x} and {y} and {z}   |')
-print('#>----------------------------------------<#')
+print('#>-----------<Menu>-----------<#')
+print(f'| min3 : between {x}, {y}, {z}      |')
+print(f'| max3 : between {x}, {y}, {z}      |')
+print(f'| avg3 : between {x}, {y}, {z}      |')
+print('#>----------------------------<#')
 action = input('action->')
 if action == '+':
-    print(f'{x} + {y} + {z} = {x + y + z }')
-elif action == '*':
-    print(f'{x} * {y} * {z} = {x * y * z}')
+    print(f'{x} + {y} = {x + y}')
+elif action == 'avg3':
+    print(f'Avg = {(x+y+z)/3}')
+elif action == 'max3':
+    if x >= y and x >= z:
+        print(f'Max is {x}')
+    elif y >= x and y >= z:
+        print(f'Max is {y}')
+    elif z >= x and z >= y:
+        print(f'Max is {z}')
+    else:
+        print(f'All values is equil!')
+elif action == 'min3':
+    if x <= y and x <= z:
+        print(f'Min is {x}')
+    elif y <= x and y <= z:
+        print(f'Min is {y}')
+    elif z <= x and z <= y:
+        print(f'Min is {z}')
+    else:
+        print(f'All values is equil!')
+else:
+    print('Command not found!')
