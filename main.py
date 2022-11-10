05eca1b5a641b35cdf274e19fd76f40b7217ce9a
Annashevchuk <sevcukanna960@gmail.com>
2022-11-10 23:32:39 +0200

    lab2

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 try:
-    userseconds = int(input('input seconds->'))
-    d = 24 * 3600
-    if d < userseconds:
-        raise Exception('All seconds per day is less then user seconds!')
-    x = d - userseconds
-    h = int(userseconds / 3600)
-    m = int((userseconds % 3600) / 60)
-    s = int((userseconds % 3600) % 60)
+    diameter = int(input('diameter->'))
+    if diameter < 0:
+        raise Exception('diameter can not be < 0 !')
     print('#>---------<MENU>---------<#')
-    print('|  h - Seconds in hour     |')
-    print('|  m - Seconds in minutes  |')
-    print('|  s - Seconds             |')
-    print('|  other key - full time   |')
+    print('|  a - area of a circle        |')
+    print('|  p - perimeter of a circleм  |')
     print('#>------------------------<#')
     action = input('action->')
-    if action == 'h':
-        print(f'Hours to midnight = {int(diff_seconds/3600)} h')
-    elif action == 'm':
-        print(f'Minutes to midnight = {int(diff_seconds/60)} m')
-    elif action == 's':
-        print(f'Seconds to midnight = {diff_seconds} s')
+    import math
+    r = (diameter / 2)
+    if action == 'a':
+        d = (math.pi * r * r )
+        print (d)
+    elif action == 'p':
+        k = (2 * r * math.pi )
+        print(k)
     else:
-        h = int(diff_seconds/3600)
-        m = int((diff_seconds % 3600) / 60)
-        s = int((diff_seconds % 3600) % 60)
-        print(f'{h}:{m}:{s} to midnight!')
+        print('Command not found!')
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
     print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
     print(f'Name: {ex.__class__.__name__}')
