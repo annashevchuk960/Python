d58d3fee549aba0583c7cd5298d84eefcfbb2688
Annashevchuk <sevcukanna960@gmail.com>
2022-11-11 16:57:09 +0200

    lab2 change1

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 try:
-    diameter = int(input('diameter->'))
-    if diameter < 0:
-        raise Exception('diameter can not be < 0 !')
-    print('#>---------<MENU>---------<#')
-    print('|  a - area of a circle        |')
-    print('|  p - perimeter of a circleм  |')
-    print('#>------------------------<#')
+    price = float(input('price->'))
+    if 0 >= price :
+        raise Exception('price can not be < 0 ')
+    gmc = float(input('the number of game consoles->'))
+    if 0 >= gmc :
+        raise Exception('the number of game consoles can not be < 0 ')
+    discount = float(input('discount->'))
+    if 0 > discount :
+        raise Exception('discount can not be < 0 ')
+    print('#>-----------<Menu>-----------<#')
+    print(f'| t - total amount              |')
+    print(f'| d - game console at a discount |')
+    print(f'| k - total amount at a discount |')
+    print('#>----------------------------<#')
     action = input('action->')
-    import math
-    r = (diameter / 2)
-    if action == 'a':
-        d = (math.pi * r * r )
-        print (d)
-    elif action == 'p':
-        k = (2 * r * math.pi )
+    if action == 't':
+        t = (price * gmc)
+        print(t)
+    elif action == 'd':
+        d = (price  * (discount / 100))
+        print(d)
+    elif action == 'k':
+        k = (price  * (discount / 100) * gmc )
         print(k)
     else:
         print('Command not found!')
@@ -21,5 +29,4 @@ except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
     print(f'Error: {ex}')
-    print(f'Name: {ex.__class__.__name__}')
     print(f'Name: {ex.__class__.__name__}')
