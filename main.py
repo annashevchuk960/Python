2084d09849b02726f55e35cf41e085b880cd82db
Annashevchuk <sevcukanna960@gmail.com>
2022-11-11 20:02:55 +0200

    lab4 change 3

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ try:
     speed = float(input('speed->'))
     if 0 >= speed :
         raise Exception('speed can not be <= 0  ')
+    size2 = (size / 125)
+    gigabyte = (speed / 1000000000)
+    print(gigabyte )
+    print(size2)
     print('#>-----------<MENU>------------<#')
     print('|  Show hour for {size} |')
     print('|  Show minute for {size}|')
@@ -12,15 +16,15 @@ try:
     print('|  other key - full time        |')
     print('#>------------------------------<#')
     action = input('action->')
-    if action == 'hour':
-        t = (price * gmc)
-        print(t)
-    elif action == 'minute':
-        d = (price  * (discount / 100))
-        print(d)
-    elif action == 'second':
-        s = (size / speed)
+    if action == 'second':
+        s = (size / gigabyte  * 1.1)
         print(s)
+    elif action == 'minute':
+        m = (size / gigabyte / 60  * 1.1)
+        print(m)
+    elif action == 'hour':
+        h = (size / gigabyte / 60 / 60  * 1.1)
+        print(h)
     else:
         print('Command not found!')
 except ValueError as vl_ex:
