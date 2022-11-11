3761a2ca7ef1003f73e9fa0c0e77f54ca5c7972b
Annashevchuk <sevcukanna960@gmail.com>
2022-11-11 23:14:09 +0200

    hm2

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 try:
-    day = int(input("Day="))
-    if day <= 0:
-        raise Exception('day can not be <= 0 ')
-    if 7 < day:
-        raise Exception('day can not be more than 7')
-    if  day == 1:
-        print("Понеділок")
-    elif day == 2:
-        print("Вівторок")
-    elif day == 3:
-        print("Середа")
-    elif day == 4:
-        print("Четвер")
-    elif day == 5:
-        print("П'ятниця")
-    elif day == 6:
-        print("Субота")
-    elif day == 7:
-        print("Неділя")
+    month = int(input("Day="))
+    if month <= 0:
+        raise Exception('month can not be <= 0 ')
+    if 12 < month:
+        raise Exception('month can not be more than 12')
+    if  month == 1:
+        print("Січень")
+    elif month == 2:
+        print("Лютий")
+    elif month == 3:
+        print("Березень")
+    elif month == 4:
+        print("Квітень")
+    elif month == 5:
+        print("Травень")
+    elif month == 6:
+        print("Червень")
+    elif month == 7:
+        print("Липень")
+    elif  month == 8:
+        print("Серпень")
+    elif month == 9:
+        print("Вересень")
+    elif month == 10:
+        print("Жовтень")
+    elif month == 11:
+        print("Листопад")
+    elif month == 12:
+        print("Грудень")
     else:
         print('Command not found!')
 except ValueError as vl_ex:
