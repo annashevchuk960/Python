18021d719290c357b7ffe83a6f531b0edd6a07e6
Annashevchuk <sevcukanna960@gmail.com>
2022-11-11 09:30:54 +0200

    lab1

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ try:
     h = int(userseconds / 3600)
     m = int((userseconds % 3600) / 60)
     s = int((userseconds % 3600) % 60)
-    print('#>---------<MENU>---------<#')
-    print('|  h - Seconds in hour     |')
-    print('|  m - Seconds in minutes  |')
-    print('|  s - Seconds             |')
-    print('|  other key - full time   |')
-    print('#>------------------------<#')
+    print('#>-----------<MENU>------------<#')
+    print('|   Show hour for {userseconds} |')
+    print('|  Show minute for {userseconds}|')
+    print('|  Show second for {userseconds}|')
+    print('|  other key - full time        |')
+    print('#>------------------------------<#')
     action = input('action->')
-    if action == 'h':
-        print(f'Hours to midnight = {int(diff_seconds/3600)} h')
-    elif action == 'm':
-        print(f'Minutes to midnight = {int(diff_seconds/60)} m')
-    elif action == 's':
-        print(f'Seconds to midnight = {diff_seconds} s')
+    if action == 'hour':
+        y = (int(x / 3600))
+        print(y)
+    elif action == 'minute':
+        d = (int(x / 60))
+        print(d)
+    elif action == 'second':
+        print (userseconds)
     else:
         h = int(diff_seconds/3600)
         m = int((diff_seconds % 3600) / 60)
