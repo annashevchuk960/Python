1b7573c5f567cbd45a1848cb31ea5610ccd73bbc
Annashevchuk <sevcukanna960@gmail.com>
2022-11-23 13:49:11 +0200

    lab2

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 try:
-    number = int(input("Number="))
-    if number <= 0:
+    number = int(input('number->'))
+    if 0 >= number :
         raise Exception('number can not be <= 0 ')
-    if number > 100:
-        raise Exception('number can not be > 100 ')
-    if  number % 3 == 0 and number % 5==0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz ")
-    elif number % 5 == 0:
-        print("Buzz")
+    print('#>-----<MENU>------<#')
+    print('|  Show degree0 |')
+    print('|  Show degree1 |')
+    print('|  Show degree2 |')
+    print('|  Show degree3 |')
+    print('|  Show degree4 |')
+    print('|  Show degree5 |')
+    print('|  Show degree6 |')
+    print('|  Show degree7 |')
+    print('#>------------------<#')
+    action = input('action->')
+    if action == 'degree1':
+        degree1=number
+        print(degree1)
+    elif action == 'degree0':
+        degree0= (1)
+        print(1)
+    elif action == 'degree2':
+        degree2= (number * number)
+        print(degree2)
+    elif action == 'degree3':
+        degree3= (number * number * number)
+        print(degree3)
+    elif action == 'degree4':
+        degree4= (number * number * number * number)
+        print(degree4)
+    elif action == 'degree5':
+        degree5= (number * number * number * number * number)
+        print(degree5)
+    elif action == 'degree6':
+        degree6= (number * number * number * number * number * number)
+        print(degree6)
+    elif action == 'degree7':
+        degree7= (number * number * number * number * number* number * number)
+        print(degree7)
     else:
-        print(number)
+        print(f'Comand not found')
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
-except Exception as ex:
-    print(f'Error: {ex}')
