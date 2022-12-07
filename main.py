5815c93f6920203bc6fc16c70b981df125a2fcc6
Annashevchuk <sevcukanna960@gmail.com>
2022-12-07 21:50:35 +0200

    lab1 2

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ try:
     for item in range(begin, end + 1):
         if item % 7 == 0:
             print(item, end="\t")
+except ValueError as vl_ex:
+    print(f'Value error: {vl_ex}')
+except Exception as ex:
+    print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
