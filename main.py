8705a680e6c0b8e5e962516ecc3edeabb199f56f
Annashevchuk <sevcukanna960@gmail.com>
2022-12-08 11:43:56 +0200

    hm4 1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 try:
     begin = int(input('begin->'))
     end = int(input('end->'))
-    for item in range(begin + 1, end):
-        if item % 2 == 0:
+    for item in reversed(range(begin, end + 1)):
             print(item, end="\t")
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
