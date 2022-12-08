599af82d67123864451fedd451f93bf836df1701
Annashevchuk <sevcukanna960@gmail.com>
2022-12-08 11:41:32 +0200

    hm2 1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 try:
     begin = int(input('begin->'))
     end = int(input('end->'))
-    for item in range(begin, end + 1):
+    for item in range(begin + 1, end):
+        if item % 2 :
             print(item, end="\t")
-
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
