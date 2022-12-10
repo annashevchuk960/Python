bba7d793dd92daad223edb7f860b1517e1652100
Annashevchuk <sevcukanna960@gmail.com>
2022-12-10 22:26:56 +0200

    lab2 ex2

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 try:
-    number = 0
-    trigger = True
-    while True:
-        number = int(input('number->'))
-        if number == 7:
-            print("Good bye")
-            break
-        else:
-            if 0 < number != 7:
-                print("Number is positive")
-            if 0 > number:
-                print("Number is negatigve")
-            if number == 0:
-                print("Number is equal to zero")
+    length = int(input('length->'))
+    sign = (input('sign->'))
+    for item in range(1,length + 1):
+     print(sign)
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
