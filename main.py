2ac5b2ab712e6582dcef6d2e7241acf65ec56a0e
Annashevchuk <sevcukanna960@gmail.com>
2022-12-10 20:40:04 +0200

    lab 2

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 try:
     begin = int(input('begin->'))
     end = int(input('end->'))
+    counter = 0
     for item in range(begin, end + 1):
-        count = 0
-        if (item % 5 == 0):
-            count += 1
-            с = ((count + item))
-            print(с)
+            print(item, end="\t")
+    print()
+    for item in range(end, begin - 1, -1):
+        print( item, end="\t")
+    print()
     for item in range(begin, end + 1):
         if item % 7 == 0:
             print(item, end="\t")
+    print()
     for item in range(begin, end + 1):
-            print(item, end="\t")
-    for item in range(end, begin - 1, -1):
-        print( item, end="\t")
+        count = 0
+        if item % 5 == 0:
+            counter += 1
+    print (counter)
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
