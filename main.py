c90565749b1ca33e499dbb71473e19ca84f76b11
Annashevchuk <sevcukanna960@gmail.com>
2022-12-10 22:26:04 +0200

    lab2 ex3

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    number = 0
+    trigger = True
+    while True:
+        number = int(input('number->'))
+        if number == 7:
+            print("Good bye")
+            break
+        else:
+            if 0 < number != 7:
+                print("Number is positive")
+            if 0 > number:
+                print("Number is negatigve")
+            if number == 0:
+                print("Number is equal to zero")
+except ValueError as vl_ex:
+    print(f'Value error: {vl_ex}')
+except Exception as ex:
+    print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
