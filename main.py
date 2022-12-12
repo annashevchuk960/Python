d5a7cc5a151058a0f969343e4eed91126fe68615
Annashevchuk <sevcukanna960@gmail.com>
2022-12-12 12:49:40 +0200

    hm2 ex4

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 try:
     length = int(input('length->'))
-    sign = ("*")
+    sign = (input('sign->'))
     for item in range(1,length + 1):
-     print (sign,end="\t")
+     print  (sign,end="\t")
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
