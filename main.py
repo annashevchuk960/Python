ac33f6906cbb82daa235fdeebed9878eb5e0f1e4
Annashevchuk <sevcukanna960@gmail.com>
2022-12-20 16:50:54 +0200

    hm3 ex3

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 try:
-    sum = 0
-    for item in range (100 , 999 + 1):
-        n1= item/100
-        n2=(item/10)%10
-        n3=item % 10
-    if n1 == n2 == n3:
-        sum += 1
-    print(sum)
+    count1 = 0
+    count2 = 0
+    for a in range(100, 9999):
+        n1 = (a // 1000)
+        n2 = (a // 100) % 10
+        n3 = (a % 10) // 10
+        if a <= 1000:
+            (n2 != n3 or n2 != n4 or n3 != n4)
+            count1 += 1
+            print(count1)
+        if a >= 1000:
+            (n1 != n2 or n1 != n3 or n1 != n4 or n2 != n3 or n2 != n4 or n3 != n4)
+            count2 += 1
+            print(count2)
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
