c58522eaad83a60020c6f78d692e8ff85efcf0f5
Annashevchuk <sevcukanna960@gmail.com>
2022-12-20 16:47:06 +0200

    hm3 ex2

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 try:
-    number = int(input('number->'))
-    number1 = int(input('number1->'))
-    answer = (number ** number1)
-    print(answer)
+    sum = 0
+    for item in range (100 , 999 + 1):
+        n1= item/100
+        n2=(item/10)%10
+        n3=item % 10
+    if n1 == n2 == n3:
+        sum += 1
+    print(sum)
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
