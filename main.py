0e5ea4edee3fa307d272392e50a210f6e665b6b5
Annashevchuk <sevcukanna960@gmail.com>
2022-12-28 22:05:47 +0200

    lab3 ex3

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ try:
         n3 = (a % 10) // 10
         n4 = a % 10
         if a <= 1000:
-            (n1 != n2 and n1 != n3 and n1 != n4 and n2 != n3 and n2 != n4 and n3 != n4)
+            (n2 != n3 or n2 != n4 or n3 != n4)
             count1 += 1
         if a >= 1000:
-            (n1 != n2 and n1 != n3 and n1 != n4 and n2 != n3 and n2 != n4 and n3 != n4)
+            (n1 != n2 or n1 != n3 or n1 != n4 or n2 != n3 or n2 != n4 or n3 != n4)
             count2 += 1
     print(count1+count2)
 except ValueError as vl_ex:
