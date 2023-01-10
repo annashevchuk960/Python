c8ed7b513a3249706d3f4fc1c6d54c9e0f81014c
Annashevchuk <sevcukanna960@gmail.com>
2023-01-10 16:55:56 +0200

    lab4 ex1

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 try:
-    count1 = 0
-    count2 = 0
-    for a in range(100, 9999):
-        n1 = (a // 1000)
-        n2 = (a // 100) % 10
-        n3 = (a % 10) // 10
-        n4 = a % 10
-        if a <= 1000:
-            (n2 != n3 or n2 != n4 or n3 != n4)
-            count1 += 1
-        if a >= 1000:
-            (n1 != n2 or n1 != n3 or n1 != n4 or n2 != n3 or n2 != n4 or n3 != n4)
-            count2 += 1
-    print(count1+count2)
+    length = int(input('length->'))
+    sign = (input('sign->'))
+    for item in range(1,length + 1):
+        s = (sign * length)
+        print  (s)
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
