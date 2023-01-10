1a4f943006bda065b46928f9e715d23c5407a642
Annashevchuk <sevcukanna960@gmail.com>
2023-01-10 17:20:35 +0200

    lab4 ex3

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 try:
-    height = int(input('height->'))
-    width = int(input('width->'))
-    sign = (input('sign->'))
-    for item in range(height ,width + 1):
-        rectangle = (sign * height and sign * width)
-        print  (rectangle)
+    number = int(input("length"))
+    sign = (input("sign"))
+    for item in range(number):
+        for j in range(number):
+            if item  == 0 or j == 0 or item  == number - 1 or j == number - 1:
+                print(sign, end="")
+            else:
+                print(" ", end="")
+        print("")
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
