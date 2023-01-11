2630c60e67de7fa01fd9ba4bb89e0794a18aaa9c
Annashevchuk <sevcukanna960@gmail.com>
2023-01-11 22:53:23 +0200

    hm4 ex1

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 try:
-    height = int(input('height->'))
-    width = int(input('width->'))
-    sign = (input('sign->'))
-    for item in range(0,height):
-        for j in range(0, width):
-            if item == 0 or item == height- 1:
-                print(sign, end=" ")
-            else:
-                if j == 0 or j == width- 1:
-                    print(sign , end=" ")
-                else:
-                    print(" ", end=" ")
-        print()
-        rectangle = (sign * height and sign * width)
+    start = int(input("START="))
+    finish = int(input("FINISH="))
+    for item in range(start , finish):
+        for j in range(2, item):
+            if item % j == 0 and item != 0:
+                print(item )
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
+    print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
     print(f'Error: {ex}')
     print(f'Name: {ex.__class__.__name__}')
