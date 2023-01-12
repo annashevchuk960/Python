a5df0bf16d5dbc0882bbf18075dd17059b58449a
Annashevchuk <sevcukanna960@gmail.com>
2023-01-12 17:22:35 +0200

    lab ex2

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 string = input("Рядок ")
-letcount = 0
-numcount = 0
-for symbol in string:
-    if symbol.isalpha():
-        letcount += 1
-    if symbol.isdigit():
-        numcount += 1
-print('Літери', letcount )
-print('Цифри', numcount)
+print(len([i for i in string if i.isdigit()]))
+print(len([i for i in string if i.isalpha()]))
