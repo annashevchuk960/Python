251d8f284ab450df8ad8a4c98ea9959236a2136b
Annashevchuk <sevcukanna960@gmail.com>
2023-01-12 17:12:39 +0200

    lab ex3

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 string = input("Рядок ")
 symbol = input("Символ ")
-counter = 0
-for s in string:
-    if symbol == s :
-        counter += 1
-print (counter)
+count = string.count(symbol)
+print (count)
