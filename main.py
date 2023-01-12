8b2cd194736b854b14558d4a2770cb04444a9efe
Annashevchuk <sevcukanna960@gmail.com>
2023-01-12 16:59:56 +0200

    lab ex2

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 string = input("Рядок ")
-print(string [::-1])
+letcount = 0
+numcount = 0
+for symbol in string:
+    if symbol.isalpha():
+        letcount += 1
+    if symbol.isdigit():
+        numcount += 1
+print('Літери', letcount )
+print('Цифри', numcount)
