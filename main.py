a322b4041d87679ee3212bf94bc256ed579d7258
Annashevchuk <sevcukanna960@gmail.com>
2023-01-19 12:28:02 +0200

    hm4 ex3

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 try:
+    number = int(input("number "))
     for i in range(1 ,10+1):
         for j in range(1 ,10+1):
-         print(f"{i} * {j} = {i *j}")
+            if number == i:
+                print(f'|{i} * {j} = {i * j}|')
+            else:
+                print(f"{i} * {j} = {i * j}")
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
 except Exception as ex:
